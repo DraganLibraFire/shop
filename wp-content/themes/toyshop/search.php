dcac6c1237960e27eb90c87db57020a01bcd26bf
Dusan LF <dusan@librafire.com>
2017-11-21 12:48:45 +0100

    fix add to cart on related products and search page style (desktop)

--- wp-content/themes/toyshop/search.php
+++ wp-content/themes/toyshop/search.php
@@ -6,40 +6,60 @@
  */
 
 get_header(); ?>
+<div class="row">
+	<?php get_sidebar(); ?>
 
-	<section id="primary" class="content-area">
+	<section id="primary" class="content-area col-md-9">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+			<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'shop' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+				<header class="page-header">
+					<h1 class="page-title"><?php printf( esc_html__( 'ZOEKRESULTATEN VOOR: %s', 'shop' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+				</header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+				<?php /* Start the Loop */ ?>
+				<ul class="products">
+					<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-				?>
+						<?php
+						/**
+						 * Run the loop for the search to output the results.
+						 * If you want to overload this in a child theme then include a file
+						 * called content-search.php and that will be used instead.
+						 */
+//					get_template_part( 'template-parts/content', 'search' );
+						get_template_part( 'woocommerce/content', 'product' );
+						?>
 
-			<?php endwhile; ?>
+					<?php endwhile; ?>
+				</ul>
 
-			<?php the_posts_navigation(); ?>
+				<div class=" nav-pagination">
+					<?php
+					global $wp_query;
 
-		<?php else : ?>
+					$big = 999999999; // need an unlikely integer
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+					echo paginate_links( array(
+							'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+							'format' => '?paged=%#%',
+							'current' => max( 1, get_query_var('paged') ),
+							'total' => $wp_query->max_num_pages,
+							'prev_text'          => __('<i class="fa fa-angle-double-left" aria-hidden="true"></i>'),
+							'next_text'          => __('<i class="fa fa-angle-double-right" aria-hidden="true"></i>')
+					) );
+					?>
+				</div>
 
-		<?php endif; ?>
+			<?php else : ?>
+
+				<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+			<?php endif; ?>
 
 		</main><!-- #main -->
 	</section><!-- #primary -->
+</div>
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- wp-content/themes/toyshop/template-parts/content-search.php
+++ wp-content/themes/toyshop/template-parts/content-search.php
@@ -11,7 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		<?php the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
@@ -23,9 +23,5 @@
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
-
-	<footer class="entry-footer">
-		<?php starter_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
 
